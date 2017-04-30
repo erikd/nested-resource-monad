1cae18c0debed9873300a95439537cd43ec5f610
Erik de Castro Lopo <erikd@mega-nerd.com>
2017-04-30 17:01:54 +1000

    Add travis build

---
+++ Scripts/travis-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -eu
+
+(cd nested-resource && \
+  cabal-1.24 configure --enable-tests && \
+  cabal-1.24 build && \
+  dist/build/test/test && \
+  cabal-1.24 haddock && \
+  cabal-1.24 sdist)
+
+(cd nested-resource-linux && \
+  cabal-1.24 configure --enable-tests && \
+  cabal-1.24 build && \
+  cabal-1.24 haddock && \
+  cabal-1.24 sdist)
